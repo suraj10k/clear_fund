de9734fa04992d20dffee3d4e468a810dbfbea36
Samarthya <samarthya55@gmail.com>
2022-01-08 14:18:11 +0530

    new post route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Link, Redirect, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Footer from "./components/Navigation/Footer";
 import Navbar from "./components/Navigation/Navbar";
+import Newpost from "./components/posts/Newpost";
 import PostDetailed from "./components/posts/PostDetailed";
 import Posts from "./components/posts/Posts";
 import UserPosts from "./components/posts/UserPosts";
 import TopBanner from "./components/Utility/TopBanner";
 import { authContext } from "./contexts/auth-context";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
 
 const App = () => {
   const { user } = useContext(authContext);
@@ -30,8 +40,21 @@ const App = () => {
           <Route exact path="/posts/:post_id">
             <PostDetailed />
           </Route>
+          <Route exact path="/new-post">
+            <Newpost />
+          </Route>
           <Redirect to="/" />
         </Switch>
+        {user && (
+          <Link to="/new-post">
+            <Box sx={{ position: "fixed", right: "40vw", bottom: "40px" }}>
+              <Fab variant="extended" color="primary" aria-label="add">
+                <AddIcon />
+                &nbsp; &nbsp; add new project/campaign
+              </Fab>
+            </Box>
+          </Link>
+        )}
         <Footer />
       </div>
     </Router>
---
+++ src/components/posts/Newpost.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Typography } from "@mui/material";
+
+export default function FormPropsTextFields() {
+  return (
+    <>
+      <Typography variant="h4" mt={2} ml={5}>
+        Add new project
+      </Typography>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "75ch" },
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "auto auto",
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextField
+            required
+            id="title"
+            label="Project title"
+            defaultValue="Hello World"
+          />
+          <TextField
+            required
+            id="description"
+            label="Project description"
+          />
+          <TextField
+            id="wallet-address"
+            label="Wallet address"
+            required
+          />
+        </div>
+      </Box>
+    </>
+  );
+}
